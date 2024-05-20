8570332909823fb010f91ea9625e2c4383d911e5
HslZsx <HslZsx@cyber-dojo.org>
2024-05-20 07:15:06 +0000

    9 ran tests, predicted green, got green

--- files/friday13_test.go
+++ files/friday13_test.go
@@ -9,6 +9,10 @@ func TestCountsShouldNotBeEmpty(t *testing.T) {
     assert.NotEmpty(t, count13s(2000))
 }
 
+func TestCountsShouldHaveFridayKey(t *testing.T) {
+    assert.Contains(t, count13s(2000), "Friday")    
+}
+
 func TestWeekDay(t *testing.T) {
     assert.Equal(t, "Friday", weekDay(2024, 5, 17))
 }
